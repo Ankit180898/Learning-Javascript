511544aa48f6608cb7f642f61c8caca8d1aaf382
Ankit180898 <karan180898@gmail.com>
2025-04-10 23:38:04 +0530

    Add JavaScript basics examples for variable declaration and concatenation

---
+++ app.js
@@ -0,0 +1,23 @@
+// LET CONST VAR
+
+//  using var
+var value='Some value';
+
+// let
+let name;
+
+// const - cant reassign 
+const nam = 'John Doe';
+console.log(nam);
+
+
+// concatenation
+let firstName = 'John';
+let lastName = 'Doe';
+console.log(firstName + ' ' + lastName);
+
+// Numbers
+// Loosely typed = dont need to declare type
+let age = 30;
+let height = 6.2;
+console.log(age+height);
