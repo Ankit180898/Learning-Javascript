5e232f88c2306acd1a2b19ac222850ccd70316c7
Ankit180898 <karan180898@gmail.com>
2025-05-27 23:03:18 +0530

    Enhance Todo List functionality with due date input, local storage support, and improved display; add CSS styling for better layout

--- scripts/11-tasks.js
+++ scripts/11-tasks.js
@@ -42,5 +42,89 @@ console.log(increaseNums);
 
 // 11h
 function addOne(arr){
-    
+    for(let i =0;i<arr.length;i++){
+        arr[i] += 1;
+
+    }
+    console.log(arr);
 }
+addOne([1,2,3]);
+
+// 11i
+function addNum(array,num){
+    for(let i =0;i<array.length;i++){
+        array[i]=array[i]+num;
+    }
+    console.log(array);
+}
+addNum([1,2,3],5);
+
+// 11j
+function addArrays(array1,array2){
+    for(let i =0;i<array1.length;i++){
+        array1[i] += array2[i];
+    }
+    console.log(array1);
+}
+addArrays([1,2,3],[4,5,6]);
+
+// 11k
+function countPositive(nums){
+    let count =0;
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]>0){
+            count++;
+        }
+    }
+    console.log(count);
+
+}
+countPositive([1,-2,3,-4,5]);
+
+// 11l 11m
+function minMax(nums){
+    const result = {
+        min: nums[0],
+        max: nums[0]
+     }
+    if(nums.length===0){
+        result.min = null;
+        result.max = null;
+        console.log(result);
+        return;
+    }
+    else if(nums.length===1){
+        result.min = nums[0];
+        result.max = nums[0];
+        console.log(result);
+        return;
+    }
+   
+     for(let i =0;i<nums.length;i++){
+        if(nums[i]<result.min){
+            result.min = nums[i];
+        }
+        else if(nums[i]>result.max){
+            result.max = nums[i];
+        }
+     }
+        console.log(result);
+}
+minMax([3]);
+
+// 11n
+function countWords(words){
+    const result = {};
+    for(let i =0;i<words.length;i++){
+        if(result[words[i]]){
+            result[words[i]]++;
+        }
+    
+        else{
+            result[words[i]] = 1;
+        }
+    }
+    console.log(result);
+}
+countWords(['apple', 'banana', 'apple', 'orange', 'banana', 'apple']);
+
--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,28 +1,60 @@
-let todoList = [];
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+;
+
 displayTodos();
 function addTodo() {
-    const todoElement = document.querySelector('.js-todo-input').value;
-    console.log(todoElement);
-    todoList.push(todoElement);
-    displayTodos();
+    const inputElement = document.querySelector('.js-todo-input').value;
+    const dueDateElement = document.querySelector('.js-due-date-input').value;
+    if (inputElement.trim() === '' || dueDateElement.trim() === '') {
+        alert('Please enter a todo and a due date');
+        return;
+    }
+    todoList.push({
+        name: inputElement,
+        dueDate: dueDateElement
+    });
+    localStorage.setItem('todoList', JSON.stringify(todoList));
     clearInput();
+
+    displayTodos();
 
 }
 function displayTodos() {
+    
     let todoListElement = document.querySelector('.js-display-todos');
     todoListElement.innerHTML = '';
+ 
     for (let i = 0; i < todoList.length; i++) {
-        todoListElement.innerHTML += `<p>${todoList[i]}</p> <button onclick="removeTodo(${i})">Remove</button>`;
+        const todoObject = todoList[i];
+        // const { name } = todoObject;
+        // const { dueDate } = todoObject;
+        const { name, dueDate } = todoObject; // Destructuring assignment
+        if( !name || !dueDate) {
+            continue; // Skip empty todos
+        }
+        todoListElement.innerHTML += `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button" onclick="removeTodo(${i},1)
+         displayTodos();
+        ">Delete
+        </button>
+        `;
     }
 }
 
-function removeTodo(index) {
-    todoList.splice(index, 1);
+function removeTodo(index, count) {
+
+    todoList.splice(index,count);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
     displayTodos();
 }
 
 function clearInput() {
     document.querySelector('.js-todo-input').value = '';
+    document.querySelector('.js-due-date-input').value = '';
+    // Refocus the input field after clearing
+    // so the user can continue adding todos without clicking again
     document.querySelector('.js-todo-input').focus();
 }
 
