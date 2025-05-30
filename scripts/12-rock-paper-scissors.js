5988f87cabc13ef5556c8d72376e47eff87611d8
Ankit180898 <karan180898@gmail.com>
2025-05-30 23:31:18 +0530

    Add Todo List functionality with due date input, local storage support, and display; implement styling for better layout

---
+++ scripts/12-rock-paper-scissors.js
@@ -0,0 +1,129 @@
+ // global variables - can be accessed
+      // outside the function
+      let result = "";
+      let computerMove = "";
+      let score =
+        // check if score is already in local storage
+        JSON.parse(localStorage.getItem("score")) || {
+          wins: 0,
+          losses: 0,
+          ties: 0,
+        };
+      updateScoreElement();
+
+      /*  
+    if (!score) {
+      // if not, initialize it
+      score = {
+        wins: 0,
+        losses: 0,
+        ties: 0,
+      };
+    } else {
+      // if it is, set the score to the value in local storage
+      score = {
+        wins: score.wins,
+        losses: score.losses,
+        ties: score.ties,
+      };
+    }
+      */
+
+      let isAutoPlaying = false;
+      let intervalId;
+      function autoPlay(){
+        const button = document.querySelector('.auto-play-button');
+
+        if(!isAutoPlaying ){
+          intervalId = setInterval(
+            function(){
+              const playerMove = getComputerMove();
+              getResult(playerMove);
+            },1000
+          );
+          isAutoPlaying=true;
+          button.innerText = 'Stop Playing'; // Change button text
+
+         
+        }
+        else{
+          clearInterval(intervalId);
+          isAutoPlaying=false;
+          button.innerText = 'Auto Play'; // Change button text
+
+         
+        }
+
+       
+      }
+
+      function getComputerMove() {
+        // function scope
+        // local variables - can't be accessed
+        // outside the function
+        const randomNumber = Math.random();
+        if (randomNumber >= 0 && randomNumber < 1 / 3) {
+          computerMove = "rock";
+        } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+          computerMove = "paper";
+        } else {
+          computerMove = "scissors";
+        }
+        return computerMove;
+      }
+      function getResult(userMove) {
+        const computerMove = getComputerMove();
+        if (userMove === "rock") {
+          if (computerMove === "scissors") {
+            result = "YOU WIN";
+          } else if (computerMove === "paper") {
+            result = "YOU LOSE";
+          } else {
+            result = "TIE";
+          }
+        } else if (userMove === "paper") {
+          if (computerMove === "rock") {
+            result = "YOU WIN";
+          } else if (computerMove === "scissors") {
+            result = "YOU LOSE";
+          } else {
+            result = "TIE";
+          }
+        } else if (userMove === "scissors") {
+          if (computerMove === "paper") {
+            result = "YOU WIN";
+          } else if (computerMove === "rock") {
+            result = "YOU LOSE";
+          } else {
+            result = "TIE";
+          }
+        }
+
+        // we update the score according to the result
+        if (result === "YOU WIN") {
+          score.wins++;
+        } else if (result === "YOU LOSE") {
+          score.losses++;
+        } else {
+          score.ties++;
+        }
+
+        // save the score to local storage
+        localStorage.setItem("score", JSON.stringify(score));
+        updateScoreElement();
+        document.querySelector(".js-result").innerHTML = result;
+        document.querySelector(
+          ".js-move"
+        ).innerHTML = `You <img src="images/${userMove}.png" class="move-icon">
+        <img src="images/${computerMove}.png" class="move-icon">
+        Computer`;
+
+        return result;
+        // it wont give anything after the return statement
+        // console.log(result);
+      }
+      function updateScoreElement() {
+        document.querySelector(
+          ".js-score"
+        ).innerHTML = ` Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+      }
---
+++ scripts/12-todo-list.js
@@ -0,0 +1,89 @@
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+displayTodos();
+function addTodo() {
+    const inputElement = document.querySelector('.js-todo-input').value;
+    const dueDateElement = document.querySelector('.js-due-date-input').value;
+    if (inputElement.trim() === '' || dueDateElement.trim() === '') {
+        alert('Please enter a todo and a due date');
+        return;
+    }
+    todoList.push({
+        name: inputElement,
+        dueDate: dueDateElement
+    });
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    clearInput();
+
+    displayTodos();
+
+}
+function displayTodos() {
+    
+    let todoListElement = document.querySelector('.js-display-todos');
+    todoListElement.innerHTML = '';
+    
+    todoList.forEach(function(todoObject,index){
+        
+        const { name, dueDate } = todoObject; // Destructuring assignment
+        if(!name || !dueDate){
+            return;
+        }
+        todoListElement.innerHTML += `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class="delete-todo-button" onclick="removeTodo(${index},1)
+         displayTodos();
+        ">Delete
+        </button>
+        `;
+    
+    });
+    // for (let i = 0; i < todoList.length; i++) {
+    //     const todoObject = todoList[i];
+    //     // const { name } = todoObject;
+    //     // const { dueDate } = todoObject;
+    //     const { name, dueDate } = todoObject; // Destructuring assignment
+    //     if( !name || !dueDate) {
+    //         continue; // Skip empty todos
+    //     }
+    //     todoListElement.innerHTML += `
+    //     <div>${name}</div>
+    //     <div>${dueDate}</div>
+    //     <button class="delete-todo-button" onclick="removeTodo(${i},1)
+    //      displayTodos();
+    //     ">Delete
+    //     </button>
+    //     `;
+    // }
+}
+
+function removeTodo(index, count) {
+
+    todoList.splice(index,count);
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+    displayTodos();
+}
+
+function clearInput() {
+    document.querySelector('.js-todo-input').value = '';
+    document.querySelector('.js-due-date-input').value = '';
+    // Refocus the input field after clearing
+    // so the user can continue adding todos without clicking again
+    document.querySelector('.js-todo-input').focus();
+}
+
+// Accumulator pattern
+const nums = [1, 1, 3];
+let total = 0;
+for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+}
+console.log(total);
+
+const numsDoubled = [];
+for (let i = 0; i < nums.length; i++) {
+    numsDoubled.push(nums[i] * 2);
+}
+console.log(numsDoubled);
+
